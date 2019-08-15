317730dcf4adfe04e898049f4dc9a40f7e234377
AJ <ascott1083@yahoo.com>
2019-08-15 13:51:18 -0500

    java work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
 // add variables and object for the four items to click
-var matchNumber;
-var wins = 0;
-var losses = 0;
+const matchNumber;
+const wins = 0;
+const losses = 0;
 
-var heros = [
+const heros = [
   {
-    name: "#mario",
+    name: "#mario"
   },
   { name: "#luigi"
   },
@@ -26,7 +26,7 @@ function startGame() {
 
 // generate a random point value for each of the items to click
 function giveHerosRandomValues() {
-  for (var i = 0; i < heros.length; i++) {
+  for (let i = 0; i < heros.length; i++) {
     heros[i].assignedValue = generateRandomNum(1, 12);
   }
 }
@@ -36,32 +36,32 @@ function giveHerosRandomValues() {
 // update the master point total for items to click
 
 $("#mario").on("click", function() {
-  var heroClick = addClicks(heros[0].assignedValue);
+  let heroClick = addClicks(heros[0].assignedValue);
   $("#hero-totals").text(heroClick);
   compareClicksToMatch(heroClick, matchNumber);
 });
 
 $("#luigi").on("click", function() {
-  var heroClick = addClicks(heros[1].assignedValue);
-  $("##hero-totals").text(heroClick);
+  let heroClick = addClicks(heros[1].assignedValue);
+  $("#hero-totals").text(heroClick);
   compareClicksToMatch(heroClick, matchNumber);
 });
 
 $("#toad").on("click", function() {
-  var heroClick = addClicks(heros[2].assignedValue);
-  $("##hero-totals").text(heroClick);
+  let heroClick = addClicks(heros[2].assignedValue);
+  $("#hero-totals").text(heroClick);
   compareClicksToMatch(heroClick, matchNumber);
 });
 
 $("#yoshi").on("click", function() {
-  var heroClick = addClicks(heros[3].assignedValue);
+  let heroClick = addClicks(heros[3].assignedValue);
   $("#hero-totals").text(heroClick);
   compareClicksToMatch(heroClick, matchNumber);
 });
 
 // general function to generate a random number
 function generateRandomNum(min, max) {
-  var num = Math.floor(Math.random() * (max - min + 1) + min);
+  let num = Math.floor(Math.random() * (max - min + 1) + min);
   return num;
 }
 
@@ -74,8 +74,8 @@ function generateMatchNumber() {
 }
 
 // create a counter function
-var addClicks = (function(clickValue) {
-  var counter = 0;
+let addClicks = (function(clickValue) {
+  let counter = 0;
   return function(clickValue) {
     return counter += clickValue;
   }
@@ -83,7 +83,7 @@ var addClicks = (function(clickValue) {
 
 // reset the counter
 function resetCounter () {
-  var myTotalScore = addClicks(0);
+  let myTotalScore = addClicks(0);
   myTotalScore = (myTotalScore * -1);
   addClicks(myTotalScore);
 }
